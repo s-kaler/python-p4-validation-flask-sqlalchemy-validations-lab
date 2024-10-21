0f2866cf85928c9e1965d5cf74d16449b1067168
Sundeep <sundeep.kaler@gmail.com>
2024-10-21 15:05:41 -0400

    complete lab

--- server/models.py
+++ server/models.py
@@ -12,6 +12,26 @@ class Author(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators 
+    @validates('name')
+    def validate_name(self, key, value):
+        if not value:
+            raise ValueError('Name is required')
+        else:
+            existing_names = [author.name for author in Author.query.all()]
+            if value in existing_names:
+                raise ValueError('Name must be unique')
+        return value
+    
+    @validates('phone_number')
+    def validate_phone_number(self, key, value):
+        if not value:
+            raise ValueError('Phone number is required')
+        elif len(value) != 10:
+            raise ValueError('Phone number must be exactly 10 digits')
+        elif not value.isdigit():
+            raise ValueError('Phone number must only contain digits')
+        return value
+    
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -28,7 +48,42 @@ class Post(db.Model):
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
     # Add validators  
+    @validates('title')
+    def validate_title(self, key, value):
+        valid_keywords = ["Won't Believe", "Secret", "Top", "Guess"]
+        is_keyword = False
+        for keyword in valid_keywords:
+            if keyword in value:
+                is_keyword = True
+        if not value:
+            raise ValueError('Title is required')
+        elif not is_keyword:
+            raise ValueError('Title must be one of: Won\'t Believe, Secret, Top, Guess')
+        return value
+    
+    @validates('content')
+    def validate_content(self, key, value):
+        if not value:
+            raise ValueError('Content is required')
+        elif len(value) < 250:
+            raise ValueError('Content must be at least 250 characters long')
+        return value
 
+    @validates('summary')
+    def validate_summary(self, key, value):
+        if not value:
+            raise ValueError('Summary is required')
+        elif len(value) > 250:
+            raise ValueError('Summary must be at most 250 characters long')
+        return value
+    
+    @validates('category')
+    def validate_category(self, key, value):
+        if not value:
+            raise ValueError('Category is required')
+        elif not value in ["Fiction", "Non-Fiction"]:
+            raise ValueError('Category must be one of: Fiction, Non-Fiction')
+        return value
 
     def __repr__(self):
         return f'Post(id={self.id}, title={self.title} content={self.content}, summary={self.summary})'
